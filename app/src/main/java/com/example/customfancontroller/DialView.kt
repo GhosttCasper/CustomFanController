17ae5d2512a3801424b619139663926295de0c9e
Vladimir <worldofwarkraft4@mail.ru>
2023-03-12 20:28:06 +0700

    Comments

--- app/src/main/java/com/example/customfancontroller/DialView.kt
+++ app/src/main/java/com/example/customfancontroller/DialView.kt
@@ -3,7 +3,6 @@ package com.example.customfancontroller
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
 import android.view.accessibility.AccessibilityNodeInfo
 import androidx.core.content.withStyledAttributes
@@ -14,6 +13,7 @@ import kotlin.math.cos
 import kotlin.math.min
 import kotlin.math.sin
 
+// Fan speeds with index of translatable string for label names
 private enum class FanSpeed(val label: Int) {
     OFF(R.string.fan_off),
     LOW(R.string.fan_low),
@@ -28,14 +28,8 @@ private enum class FanSpeed(val label: Int) {
     }
 }
 
-private const val RADIUS_OFFSET_LABEL = 30
-private const val RADIUS_OFFSET_INDICATOR = -35
-
-private var radius = 0.0f                   // Radius of the circle.
-private var fanSpeed = FanSpeed.OFF         // The active selection.
-
-// position variable which will be used to draw label and indicator circle position
-private val pointPosition: PointF = PointF(0.0f, 0.0f)
+private const val RADIUS_OFFSET_LABEL = 30          //Offset from dial radius to draw text label
+private const val RADIUS_OFFSET_INDICATOR = -35     //Offset from dial radius to draw indicator
 
 class DialView @JvmOverloads constructor(
     context: Context,
@@ -44,11 +38,21 @@ class DialView @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        // Paint styles used for rendering are initialized here. This
+        // is a performance optimization, since onDraw() is called
+        // for every screen refresh.
         style = Paint.Style.FILL
         textAlign = Paint.Align.CENTER
         textSize = 55.0f
         typeface = Typeface.create("", Typeface.BOLD)
     }
+
+    private var radius = 0.0f                  // Radius of the circle.
+    private var fanSpeed = FanSpeed.OFF        // The active selection.
+
+    //Point at which to draw label and indicator circle position. PointF is a point
+    //with floating-point coordinates.
+    private val pointPosition: PointF = PointF(0.0f, 0.0f)
 
     private var fanSpeedLowColor = 0
     private var fanSpeedMediumColor = 0
@@ -63,6 +67,10 @@ class DialView @JvmOverloads constructor(
             fanSpeedMaxColor = getColor(R.styleable.DialView_fanColor3, 0)
         }
 
+        updateContentDescription()
+
+        // For minsdk >= 21, you can just add a click action. In this app since minSdk is 19,
+        // you must add a delegate to handle accessibility.
         ViewCompat.setAccessibilityDelegate(this, object : AccessibilityDelegateCompat() {
             override fun onInitializeAccessibilityNodeInfo(
                 host: View,
@@ -71,9 +79,12 @@ class DialView @JvmOverloads constructor(
                 super.onInitializeAccessibilityNodeInfo(host, info)
                 val customClick = AccessibilityNodeInfoCompat.AccessibilityActionCompat(
                     AccessibilityNodeInfo.ACTION_CLICK,
+                    // If the fan speed is OFF, LOW, or MEDIUM, the hint is to change the speed.
+                    // If it is HIGH use reset.
                     context.getString(
                         if (fanSpeed != FanSpeed.HIGH)
-                            R.string.change else R.string.reset
+                            R.string.change
+                        else R.string.reset
                     )
                 )
                 info.addAction(customClick)
@@ -83,35 +94,44 @@ class DialView @JvmOverloads constructor(
     }
 
     override fun performClick(): Boolean {
-        Log.w("MY_TAG", "performClick")
+        // Give default click listeners priority and perform accessibility/autofill events.
+        // Also calls onClickListener() to handle further subclass customizations.
         if (super.performClick()) return true
 
+        // Rotates between each of the different selection
+        // states on each click.
         fanSpeed = fanSpeed.next()
-        contentDescription = resources.getString(fanSpeed.label)
-
         updateContentDescription()
+        // Redraw the view.
         invalidate()
         return true
     }
 
+    /**
+     * This is called during layout when the size of this view has changed. If
+     * the view was just added to the view hierarchy, it is called with the old
+     * values of 0. The code determines the drawing bounds for the custom view.
+     *
+     * @param width    Current width of this view.
+     * @param height    Current height of this view.
+     * @param oldWidth Old width of this view.
+     * @param oldHeight Old height of this view.
+     */
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
-        Log.w("MY_TAG", "onSizeChanged")
+        // Calculate the radius from the smaller of the width and height.
         radius = (min(width, height) / 2.0 * 0.8).toFloat()
     }
 
-    private fun PointF.computeXYForSpeed(pos: FanSpeed, radius: Float) {
-        // Angles are in radians.
-        val startAngle = Math.PI * (9 / 8.0)
-        val angle = startAngle + pos.ordinal * (Math.PI / 4)
-        x = (radius * cos(angle)).toFloat() + width / 2
-        y = (radius * sin(angle)).toFloat() + height / 2
-    }
-
+    /**
+     * Renders view content: an outer circle to serve as the "dial",
+     * and a smaller black circle to server as the indicator.
+     * The position of the indicator is based on fanSpeed.
+     *
+     * @param canvas The canvas on which the background will be drawn.
+     */
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        Log.w("MY_TAG", "onDraw")
-
-        // Set dial background color to green if selection not off.
+        // Set dial background color based on the selection.
         paint.color = when (fanSpeed) {
             FanSpeed.OFF -> Color.GRAY
             FanSpeed.LOW -> fanSpeedLowColor
@@ -137,8 +157,27 @@ class DialView @JvmOverloads constructor(
         }
     }
 
-    fun updateContentDescription() {
-        contentDescription = resources.getString(fanSpeed.label)
+    /**
+     * Computes the X/Y-coordinates for a label or indicator,
+     * given the FanSpeed and radius where the label should be drawn.
+     *
+     * @param pos Position (FanSpeed)
+     * @param radius Radius where label/indicator is to be drawn.
+     * @return 2-element array. Element 0 is X-coordinate, element 1 is Y-coordinate.
+     */
+    private fun PointF.computeXYForSpeed(pos: FanSpeed, radius: Float) {
+        // Angles are in radians.
+        val startAngle = Math.PI * (9 / 8.0)
+        val angle = startAngle + pos.ordinal * (Math.PI / 4)
+        x = (radius * cos(angle)).toFloat() + width / 2
+        y = (radius * sin(angle)).toFloat() + height / 2
     }
 
+    /**
+     * Updates the view's content description with the appropirate string for the
+     * current fan speed.
+     */
+    private fun updateContentDescription() {
+        contentDescription = resources.getString(fanSpeed.label)
+    }
 }
